e9ebe474989d82cbf6732c409e299370669f79af
laiyj <laiyujun@vip.qq.com>
2025-06-09 09:14:23 +0800

    upd gcc version

--- script/Handles.sh
+++ script/Handles.sh
@@ -76,9 +76,9 @@ if [ -f "$SP_FILE" ]; then
 fi
 
 #修复freeswitch依赖缺失
-FW_FILE=$(find ../feeds/telephony/ -maxdepth 3 -type f -wholename "*/freeswitch/Makefile")
-if [ -f "$FW_FILE" ]; then
-	sed -i "s/libpcre/libpcre2/g" $FW_FILE
+#FW_FILE=$(find ../feeds/telephony/ -maxdepth 3 -type f -wholename "*/freeswitch/Makefile")
+#if [ -f "$FW_FILE" ]; then
+#	sed -i "s/libpcre/libpcre2/g" $FW_FILE
 
-	cd $PKG_PATCH && echo "freeswitch has been fixed!"
-fi
+#	cd $PKG_PATCH && echo "freeswitch has been fixed!"
+#fi
